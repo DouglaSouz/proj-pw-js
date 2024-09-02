9c26dbb9304a59888b1b1c40a7eaaf11bf215b7c
douglas <douglassg2009@gmail.com>
2024-09-02 18:27:16 -0300

    introdução ao javascript

---
+++ calculo.js
@@ -0,0 +1,10 @@
+function somar (){
+   var x = 2
+   var y = 2
+   let res = x+y
+   document.getElementById("soma").innerHTML=res
+   document.getElementById("conf-soma").innerHTML="Calculado ✔"
+
+
+
+}
