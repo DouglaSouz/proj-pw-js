0cae42fb3392f5103abcb93534a67a88ee1eca33
Vinicius Tibo <viniciustiboff@gmail.com>
2024-09-30 17:17:07 -0300

    commit

---
+++ aula05-parte 01/script.js
@@ -0,0 +1,3 @@
+window.alert("Minha primeira mensagem!");
+window.confirm("Confirmo que gosto de PW!");
+window.prompt("Qual é o seu nome?");
---
+++ aula05-parte03/script.js
@@ -0,0 +1,5 @@
+var nome = window.prompt("Digite o seu nome: ");
+var idade = Number(window.prompt("Digite sua idade: "));
+document.write(`seu nome é <b>${nome}</b> e tem <b>${idade}.</b><br>`);
+document.write(`Seu nome tem <b>${nome.length} caracteres</b>.<br>`);
+document.write(`Seu nome em letras maiusculas é <br>${idade * 365}</b>`)
